008533ffd8d316d11a4ed5985c070c6d484497f9
sathyank <ksathya93@gmail.com>
2018-04-18 18:07:29 +0530

    finding cycle in linked list

---
+++ Algorithms/cycle_in_ll.py
@@ -0,0 +1,26 @@
+/*
+Detect a cycle in a linked list. Note that the head pointer may be 'NULL' if the list is empty.
+
+A Node is defined as:
+    struct Node {
+        int data;
+        struct Node* next;
+    }
+*/
+
+bool has_cycle(Node* head) {
+
+    if(head == NULL)
+        return false;
+    Node *slow = head;
+    Node *fast = head->next;
+    while(slow!=fast)
+    {
+        if(fast == NULL || fast->next == NULL)
+            return false;
+        fast = fast->next->next;
+        slow = slow->next;
+
+    }
+    return true;
+}
